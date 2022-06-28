4ec2df1c0ecfb54bb541bdf99ca99bee39f37d15
Iseason2000 <Iseason2000@qq.com>
2022-06-28 23:57:38 +0800

    调整混淆设置

--- src/main/kotlin/top/iseason/bukkit/templateplugin/TemplatePlugin.kt
+++ src/main/kotlin/top/iseason/bukkit/templateplugin/TemplatePlugin.kt
@@ -1,21 +1,21 @@
 package top.iseason.bukkit.templateplugin
 
-import org.bukkit.ChatColor
 import top.iseason.bukkit.bukkittemplate.KotlinPlugin
 import top.iseason.bukkit.bukkittemplate.command.CommandBuilder
+import top.iseason.bukkit.bukkittemplate.config.ConfigWatcher
 import top.iseason.bukkit.bukkittemplate.debug.SimpleLogger
 import top.iseason.bukkit.bukkittemplate.debug.info
 import top.iseason.bukkit.bukkittemplate.ui.UIListener
-
 import top.iseason.bukkit.bukkittemplate.utils.toColor
 
 object TemplatePlugin : KotlinPlugin() {
 
     override fun onAsyncLoad() {
 
-//        command1()
+        command1()
 //        command3()
         openUICommand()
+
     }
 
     override fun onEnable() {
@@ -26,15 +26,14 @@ object TemplatePlugin : KotlinPlugin() {
         UIListener.onEnable()
 
         SimpleLogger.prefix = "&a[&6${javaPlugin.description.name}&a]&r ".toColor()
-        info("${ChatColor.GREEN}插件已启用!")
+        info("&a插件已启用!")
     }
 
     override fun onAsyncEnable() {
-//        Config
+        Config
     }
 
     override fun onDisable() {
-
 
         //如果使用命令模块，取消注释
         CommandBuilder.onDisable()
@@ -43,9 +42,9 @@ object TemplatePlugin : KotlinPlugin() {
         UIListener.onDisable()
 
         //如果使用配置模块，取消注销
-//        ConfigWatcher.onDisable()
+        ConfigWatcher.onDisable()
 
-        info("${ChatColor.YELLOW}插件已卸载!")
+        info("&6插件已卸载!")
     }
 
 }
