855506d50d7bc4cf8c58889219b0915c981e41bf
Iseason2000 <Iseason2000@qq.com>
2022-08-08 22:02:06 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     compileOnly("org.jetbrains.exposed:exposed-core:$exposedVersion")
     compileOnly("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     compileOnly("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    compileOnly("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
 
     implementation("org.bstats:bstats-bukkit:3.0.0")
     compileOnly("org.spigotmc:spigot-api:1.13-R0.1-SNAPSHOT")
